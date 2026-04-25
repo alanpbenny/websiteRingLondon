ffaf8ef52afc10665244662824dcea5e252a71bc
Alan Panoli Thundiyil Benny <alanbenny@Alans-MacBook-Air.local>
2026-04-25 16:21:26 -0400

    Added Analystics

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./home";
 import Membership from "./membership";
 import About from "./about";
+import { Analytics } from "@vercel/analytics/react"
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
           <Route path="/about" element={<About />} />
         </Routes>
       </BrowserRouter>
+      <Analytics />
     </div>
   );
 }
